f2fe786659f36fab2fbe1495ff6a195971a3eb79
mukeshchoudhary <mukeshaitpune@gmail.com>
2019-02-11 15:10:13 +0530

    notificaiton in android studio

---
+++ app/src/main/java/com/boolment/androidnotification/MainActivity.java
@@ -0,0 +1,58 @@
+package com.boolment.androidnotification;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String CHANNEL_ID = "personal_notification";
+    private final int NOTIFICATION_ID =  001;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //this method is applicable to display notification on android lower than 8.0
+    public void displayNotification(View view) {
+
+        //this to call notificaiton for above 8.0 version
+        createNotificationChannel();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_message);
+        builder.setContentTitle("Simple Notification");
+        //this line of text is displayed in single line if it extend then it will be trancked if you want to use large text then you have to use expandable notification
+        builder.setContentText("This is a simple notification...");
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
+
+    }
+
+    private void createNotificationChannel()
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            CharSequence name = "Personal Notification";
+            String description = "Include All the Personal Notification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,name,importance);
+            notificationChannel.setDescription(description);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+
+    }
+}
