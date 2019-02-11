1fe0e5d94a880c46bd70ce07d223018148a77b1c
mukeshchoudhary <mukeshaitpune@gmail.com>
2019-02-11 15:33:02 +0530

    notificaiton in android studio

--- app/src/main/java/com/boolment/androidnotification/MainActivity.java
+++ app/src/main/java/com/boolment/androidnotification/MainActivity.java
@@ -2,6 +2,8 @@ package com.boolment.androidnotification;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -27,13 +29,22 @@ public class MainActivity extends AppCompatActivity {
         //this to call notificaiton for above 8.0 version
         createNotificationChannel();
 
+
+        Intent landingIntent = new Intent(this,LandingActivity.class);
+        landingIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent landingPendingIntent = PendingIntent.getActivity(this,0,landingIntent,PendingIntent.FLAG_ONE_SHOT);
+
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID);
         builder.setSmallIcon(R.drawable.ic_message);
         builder.setContentTitle("Simple Notification");
         //this line of text is displayed in single line if it extend then it will be trancked if you want to use large text then you have to use expandable notification
         builder.setContentText("This is a simple notification...");
         builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
+        builder.setContentIntent(landingPendingIntent);
+        //this will auto cancell notificaiton after its open activity.
+        builder.setAutoCancel(true);
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
         notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
 
@@ -41,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void createNotificationChannel()
     {
+        //checking which version of android it is.
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
         {
             CharSequence name = "Personal Notification";
